8c7f0526321965234ad43afcee580d7b9baf6bea
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-04-07 12:29:22 -0400

    Add first pseudocode for getComputerChoice()

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// write function that gets a choice for the computer
+function getComputerChoice() {
+    //initialize variable to store random number
+    //initialize variable to store game choice
+    //create random number variable between 0 - 2
+    //write switch statement to choose game roll based on number
+}
