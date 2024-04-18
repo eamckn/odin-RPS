a78f2e173191e9fa4042f4a8a7c8d82edeaa7b58
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-04-18 19:10:17 -0400

    Add winner message and disable btns at 5 wins

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
+// ROCK PAPER SCISSORS
 
-function getComputerChoice() {
-    // Create random number variable between 0 - 2
-    let rockPaperScissorsNumber = Math.floor(Math.random() * 3);
-    console.log(rockPaperScissorsNumber);
-    // Choose game roll based on number
-    switch (rockPaperScissorsNumber) {
-        case 0:
-            return "rock";
-        case 1:
-            return "paper";
-        case 2: 
-            return "scissors";
-    }
+let playerWins = 0;
+let computerWins = 0;
+
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+
+const score = document.querySelector("#score");
+const playerScore = document.querySelector("#player-score");
+const computerScore = document.querySelector("#computer-score");
+const roundMessage = document.querySelector("#round-message");
+const declareWinner = document.querySelector("#winner");
+
+playerScore.textContent = `PLAYER SCORE: ${playerWins}`;
+computerScore.textContent = `COMPUTER SCORE: ${computerWins}`;
+
+const choicesMenu = document.querySelector("#choices");
+
+let makeChoice = function(event) {
+    let target = event.target;
+
+            switch(target.id) {
+                case "rock":
+                    playRound("rock", getComputerChoice());
+                    playerScore.textContent = `PLAYER SCORE: ${playerWins}`;
+                    computerScore.textContent = `COMPUTER SCORE: ${computerWins}`;
+                    checkWinner();
+                    break;
+                case "paper":
+                    playRound("paper", getComputerChoice())
+                    playerScore.textContent = `PLAYER SCORE: ${playerWins}`;
+                    computerScore.textContent = `COMPUTER SCORE: ${computerWins}`;
+                    checkWinner();
+                    break;
+                case "scissors":
+                    playRound("scissors", getComputerChoice())
+                    playerScore.textContent = `PLAYER SCORE: ${playerWins}`;
+                    computerScore.textContent = `COMPUTER SCORE: ${computerWins}`;
+                    checkWinner();
+                    break;
+            }
 }
+
+choicesMenu.addEventListener('click', makeChoice)
 
 // Play a round of rock paper scissors
 function playRound(playerSelection, computerSelection) {
@@ -42,60 +73,34 @@ function playRound(playerSelection, computerSelection) {
         ++computerWins;
         roundMessage.textContent = "You lose! Scissors beats paper.";
     }
-    else {
+    else if (playerSelection === "scissors" && computerSelection === "rock") {
         ++computerWins; 
         roundMessage.textContent = "You lose! Rock beats scissors.";
     }
 }
 
-function playGame() {
-    let currentScore;
-    let roundResults;
-     // Get player choice for current round
-    const playerSelection = prompt("Rock, paper, or scissors?", "").toLowerCase();        // Get  computer choice for current round
-    const computerSelection = getComputerChoice();
-    // Evaluate and store the round results
-    roundResults = playRound(playerSelection, computerSelection);
-    // If results are a tie, don't increment any score, make the loop go an additional time
-    if (roundResults === "TIE") {
-        console.log("It's a tie! Go again.");
-    }
-    else {
-        currentScore = `\nPlayer score: ${playerWins}\nComputer score: ${computerWins}`;
-        console.log(roundResults + currentScore);
+function getComputerChoice() {
+    // Create random number variable between 0 - 2
+    let rockPaperScissorsNumber = Math.floor(Math.random() * 3);
+    console.log(rockPaperScissorsNumber);
+    // Choose game roll based on number
+    switch (rockPaperScissorsNumber) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2: 
+            return "scissors";
     }
 }
 
-let playerWins = 0;
-let computerWins = 0;
-
-const rockBtn = document.querySelector("#rock");
-const paperBtn = document.querySelector("#paper");
-const scissorsBtn = document.querySelector("#scissors");
-
-const score = document.querySelector("#score");
-const playerScore = document.querySelector("#player-score");
-const computerScore = document.querySelector("#computer-score");
-const roundMessage = document.querySelector("#round-message");
-
-playerScore.textContent = `PLAYER SCORE: ${playerWins}`;
-computerScore.textContent = `COMPUTER SCORE: ${computerWins}`;
-
-
-rockBtn.addEventListener("click", () => {
-    playRound("rock", getComputerChoice());
-    playerScore.textContent = `PLAYER SCORE: ${playerWins}`;
-    computerScore.textContent = `COMPUTER SCORE: ${computerWins}`;
-})
-paperBtn.addEventListener("click", () => {
-    playRound("paper", getComputerChoice())
-    playerScore.textContent = `PLAYER SCORE: ${playerWins}`;
-    computerScore.textContent = `COMPUTER SCORE: ${computerWins}`;
-})
-scissorsBtn.addEventListener("click", () => {
-    playRound("scissors", getComputerChoice())
-    playerScore.textContent = `PLAYER SCORE: ${playerWins}`;
-    computerScore.textContent = `COMPUTER SCORE: ${computerWins}`;
-})
-
-//playGame();
+function checkWinner() {
+    if (playerWins === 5) {
+        declareWinner.textContent = "CONGRATULATIONS! YOU WIN!";
+        choicesMenu.removeEventListener('click', makeChoice)
+    }
+    else if (computerWins === 5) {
+        declareWinner.textContent = "Oh no! You lost. Better luck next time!";
+        choicesMenu.removeEventListener('click', makeChoice)
+    }
+}
